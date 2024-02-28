5ae85bd5cc6ad90d53097a2193387f4e91d9178c
pawel.sawicki <ce-pawel.sawicki@regnology.net>
2024-02-28 21:18:39 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There 3
         </p>
         <a
           className="App-link"
